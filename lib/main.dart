63baa6a4568ff2d41edf118fcc72a609244b40ae
Bibesh17 <bibeshpokharel17@gmail.com>
2024-08-19 23:27:43 +0545

    initial

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/start_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Quiz App',
+        home: Scaffold(
+          body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 187, 106, 201),
+                Color.fromARGB(255, 177, 21, 204)
+              ],
+            )),
+            child: const StartScreen(),
+          ),
+        ));
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz.png',
+            width: 300,
+          ),
+        ],
+      ),
+    );
+  }
+}
